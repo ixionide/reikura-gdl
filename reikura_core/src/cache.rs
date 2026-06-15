52549e301aba1fefc7d1876646568520d3e55775
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-06-15 16:19:54 +0700

    Update cache.rs

--- reikura_core/src/cache.rs
+++ reikura_core/src/cache.rs
@@ -20,9 +20,9 @@ impl CacheManager {
     pub fn new() -> Self {
         Self {
             image: LruCache::new(NonZeroUsize::new(100).unwrap()),
-            scene: LruCache::new(NonZeroUsize::new(20).unwrap()),
+            scene: LruCache::new(NonZeroUsize::new(24).unwrap()),
             sfx: LruCache::new(NonZeroUsize::new(32).unwrap()),
-            bgm: LruCache::new(NonZeroUsize::new(10).unwrap()),
+            bgm: LruCache::new(NonZeroUsize::new(16).unwrap()),
             voice: LruCache::new(NonZeroUsize::new(100).unwrap()),
         }
     }
