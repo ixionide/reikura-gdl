9664ecc525cd7d9d08d4c412b0c262d92a3f7e2a
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-06-17 20:03:41 +0700

    more correct implementation of calc instruction

--- reikura_core/src/instruction/calc.rs
+++ reikura_core/src/instruction/calc.rs
@@ -9,38 +9,58 @@ impl Instruction for Calc {
     fn execute(vm: &mut crate::Vm, _info: super::InstructionInfo) -> anyhow::Result<()> {
         let var_index: u16 = vm.scene.param()?;
         let mut result = 0;
+        let mut tmp = None;
 
         loop {
             match vm.scene.read_le::<u8>()? {
                 0 => {
-                    let rhs: Value = vm.scene.param()?;
-                    result += rhs.evaluate(&vm.ctx);
+                    let rhs = vm.scene.param::<Value>()?.evaluate(&vm.ctx);
+
+                    if let Some(num) = tmp.replace(rhs) {
+                        result += num;
+                    }
                 }
                 1 => {
-                    let rhs: Value = vm.scene.param()?;
-                    result -= rhs.evaluate(&vm.ctx);
+                    let rhs = vm.scene.param::<Value>()?.evaluate(&vm.ctx);
+
+                    if let Some(num) = tmp.replace(-rhs) {
+                        result += num;
+                    }
                 }
                 2 => {
-                    let rhs: Value = vm.scene.param()?;
-                    result *= rhs.evaluate(&vm.ctx);
+                    let rhs = vm.scene.param::<Value>()?.evaluate(&vm.ctx);
+
+                    if let Some(lhs) = &mut tmp {
+                        *lhs *= rhs
+                    }
                 }
                 3 => {
                     let rhs = vm.scene.param::<Value>()?.evaluate(&vm.ctx);
-                    if rhs != 0 {
-                        result /= rhs;
+
+                    if let Some(lhs) = &mut tmp
+                        && rhs != 0
+                    {
+                        *lhs /= rhs;
                     }
                 }
                 4 => {
                     let rhs = vm.scene.param::<Value>()?.evaluate(&vm.ctx);
-                    if rhs != 0 {
-                        result %= rhs;
+
+                    if let Some(lhs) = &mut tmp
+                        && rhs != 0
+                    {
+                        *lhs %= rhs;
                     }
                 }
                 5 => {
+                    if let Some(rhs) = tmp.take() {
+                        result += rhs;
+                    }
+
                     vm.ctx.variables.set(var_index as usize, result);
                     break;
                 }
-                unk @ 6.. => bail!("unknown calc operation: {unk}"),
+                unk => bail!("unknown calc operator: {unk}"),
             }
         }
 
