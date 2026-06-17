6234f5f194373a0daa2e8fabfbdfbc3377192847
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-06-17 16:41:27 +0700

    more instruction implemented

---
+++ reikura_core/src/instruction/exc.rs
@@ -0,0 +1,42 @@
+use std::cmp::min;
+
+use anyhow::bail;
+
+use crate::instruction::{Evaluate, Instruction, ReadParam, Value};
+
+pub struct Exc;
+
+impl Instruction for Exc {
+    fn execute(vm: &mut crate::Vm, _info: super::InstructionInfo) -> anyhow::Result<()> {
+        let ctx = &mut vm.ctx;
+        let Some(save) = &mut vm.save else {
+            bail!("save is not initialized yet");
+        };
+
+        let dst = vm.scene.param::<Value>()?.evaluate(ctx) as usize;
+        let src = vm.scene.param::<Value>()?.evaluate(ctx) as usize;
+        let count = vm.scene.param::<Value>()?.evaluate(ctx) as usize;
+
+        match vm.scene.param::<u8>()? {
+            0 => {
+                let bound_count = min(save.flags.len() - src, ctx.flags.len() - dst);
+
+                for i in 0..count.min(bound_count) {
+                    let flag = save.flags.get(src + i).unwrap_or(false);
+                    ctx.flags.set(dst + i, flag);
+                }
+            }
+            1 => {
+                let bound_count = min(save.variables.len() - src, ctx.variables.len() - dst);
+
+                for i in 0..count.min(bound_count) {
+                    let value = save.variables.get(src + i).unwrap_or(0);
+                    ctx.variables.set(dst + i, value);
+                }
+            }
+            _ => bail!("invalid EXC slot param"),
+        }
+
+        Ok(())
+    }
+}
