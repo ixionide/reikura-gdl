2c1782a9b940550f24d66acbb82ef688f5e9b24d
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-06-20 16:37:54 +0700

    fix typo

--- reikura_core/src/instruction/timerget.rs
+++ reikura_core/src/instruction/timerget.rs
@@ -11,11 +11,10 @@ impl Instruction for Timerget {
     fn execute(vm: &mut crate::Vm, _info: super::InstructionInfo) -> anyhow::Result<()> {
         let var_index: u16 = vm.scene.param()?;
 
-        let Some(var_value) = vm.ctx.timer.as_ref().map(Timer::get) else {
-            bail!("timer is not yet");
+        match vm.ctx.timer.as_ref().map(Timer::get) {
+            Some(var_value) => vm.ctx.variables.set(var_index as usize, var_value),
+            None => bail!("timer is not set yet"),
         };
-
-        vm.ctx.variables.set(var_index as usize, var_value);
 
         Ok(())
     }
