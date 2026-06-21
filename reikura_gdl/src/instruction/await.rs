322a6a55c50e060b246093d537ceb85c50e470dc
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-06-21 15:38:17 +0700

    leftover

--- reikura_gdl/src/instruction/await.rs
+++ reikura_gdl/src/instruction/await.rs
@@ -8,7 +8,7 @@ impl Instruction for Await {
     fn execute(vm: &mut crate::Vm, _info: super::InstructionInfo) -> anyhow::Result<()> {
         match vm.ctx.wait_duration.take() {
             Some(duration) => vm.state.wait(duration),
-            None => bail!("wait time is not set yet"),
+            None => bail!("wait duration is not set yet"),
         }
 
         Ok(())
