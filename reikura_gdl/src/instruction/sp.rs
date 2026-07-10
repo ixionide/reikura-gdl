aab3de49b6e8ca68e26bf0c2d61231eb38e8b240
ixionide <200210767+ixionide@users.noreply.github.com>
2026-07-10 19:43:10 +0700

    cleanup (#3)

--- reikura_gdl/src/instruction/sp.rs
+++ reikura_gdl/src/instruction/sp.rs
@@ -22,7 +22,7 @@ impl Instruction for Sp {
 
         let group_index: u8 = params.read_le()?;
 
-        while params.len() > 0 {
+        while !params.is_empty() {
             let byte_index_start: u16 = params.read_le()?;
             let bits: IndexVec<u16, u8> = params.read_le()?;
 
@@ -30,8 +30,8 @@ impl Instruction for Sp {
                 // XXX: this logic only work when there is only one bit that is set
                 assert_eq!(bit.count_ones(), 1);
 
-                let index = byte_index as usize * size_of::<u8>() + bit.trailing_zeros() as usize;
-                flag_groups.push(index);
+                let flag_index = byte_index as usize * 8 + bit.trailing_zeros() as usize;
+                flag_groups.push(flag_index);
             }
         }
 
