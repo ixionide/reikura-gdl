44a05de638332973a49a7685b08bb9c131c636f7
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-07-12 23:38:13 +0700

    fix ml & mp

--- reikura_gdl/src/instruction/ml.rs
+++ reikura_gdl/src/instruction/ml.rs
@@ -1,13 +1,28 @@
+use anyhow::bail;
+
 use crate::instruction::{AssetName, Instruction, ReadParam};
 
 pub struct Ml;
 
+reikura_util::const_iota! {
+    u8 = iota,
+    AUTO_PLAY,
+    NO_AUTO_PLAY,
+}
+
 impl Instruction for Ml {
     fn execute(vm: &mut crate::Vm, _info: super::InstructionInfo) -> anyhow::Result<()> {
         let name: AssetName = vm.scene.param()?;
-        let _unknown: u8 = vm.scene.param()?;
+        let cmd: u8 = vm.scene.param()?;
         let bgm = vm.assets.load_bgm(name.decode()?.as_str())?;
+
         vm.audio.bgm = Some(bgm);
+
+        match cmd {
+            AUTO_PLAY => vm.audio.play_bgm(true, None)?,
+            NO_AUTO_PLAY => (),
+            _ => bail!("unknown ML cmd: {cmd}"),
+        }
 
         Ok(())
     }
--- reikura_gdl/src/instruction/mp.rs
+++ reikura_gdl/src/instruction/mp.rs
@@ -1,18 +1,32 @@
 use std::time::Duration;
 
+use anyhow::bail;
+
 use crate::instruction::{Evaluate, Instruction, ReadParam, Value};
+
+reikura_util::const_iota! {
+    u8 = iota,
+    LOOP,
+    ONCE,
+}
 
 pub struct Mp;
 
 impl Instruction for Mp {
     fn execute(vm: &mut crate::Vm, info: super::InstructionInfo) -> anyhow::Result<()> {
-        let looping = vm.scene.param::<u8>()? == 0;
+        let cmd: u8 = vm.scene.param()?;
         let mut fade = None;
 
         if info.param_length() == 5 {
             let ms = vm.scene.param::<Value>()?.evaluate(&vm.ctx);
             fade = ms.is_positive().then(|| Duration::from_millis(ms as u64));
         }
+
+        let looping = match cmd {
+            LOOP => true,
+            ONCE => false,
+            _ => bail!("unknown MP cmd: {cmd}"),
+        };
 
         vm.audio.play_bgm(looping, fade)?;
 
