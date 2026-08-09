c377f3ad5fdb0eb6c73466e3e10b03544a1113cb
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-08-09 18:48:59 +0700

    wip

--- reikura_gdl/src/secretfilter.rs
+++ reikura_gdl/src/secretfilter.rs
@@ -12,7 +12,7 @@ pub const fn decode_base36_digit(digit: u8) -> u8 {
     }
 }
 
-pub fn decode_filter(filter: &mut [u8; 2048]) {
+pub fn decode(digits: &mut [u8]) {
     #[rustfmt::skip]
     const MAP_TABLE: [u8; 36] = [
         0x06, 0x21, 0x19, 0x10, 0x08, 0x01, 0x1E, 0x16, 0x1C, 0x07, 0x15, 0x13,
@@ -24,32 +24,59 @@ pub fn decode_filter(filter: &mut [u8; 2048]) {
         MAP_TABLE[decode_base36_digit(digit) as usize]
     }
 
-    filter.iter_mut().for_each(|byte| *byte = map(*byte));
+    digits.iter_mut().for_each(|byte| *byte = map(*byte));
 }
 
-pub fn get_key_len(filter: &[u8; 2048]) -> usize {
-    fn base36_diff(val1: u8, val2: u8) -> u8 {
-        let diff = val1.abs_diff(val2) as usize;
+fn base36_diff(val1: u8, val2: u8) -> u8 {
+    let diff = val1.abs_diff(val2) as usize;
 
-        let digit = {
-            if val1 < val2 {
-                BASE36_DIGITS[36 - diff]
-            } else {
-                BASE36_DIGITS[diff]
-            }
-        };
+    if val1 < val2 {
+        BASE36_DIGITS[36 - diff]
+    } else {
+        BASE36_DIGITS[diff]
+    }
+}
 
-        decode_base36_digit(digit)
+pub fn update_key(key: &mut [u8], pos: usize, filter: &[u8; 2048]) {
+    let filter = &filter[..0x400];
+    decode(key);
+
+    for (i, k) in key.iter_mut().enumerate() {
+        let index = (*k + filter[(pos + i) % 0x400]) % 36;
+        *k = BASE36_DIGITS[index as usize];
+    }
+}
+
+pub fn get_key(filter: &[u8; 2048]) -> Vec<u8> {
+    let hi_digit = base36_diff(filter[0x500], filter[0x100]);
+    let lo_digit = base36_diff(filter[0x501], filter[0x101]);
+    let hi = decode_base36_digit(hi_digit) << 4;
+    let lo = decode_base36_digit(lo_digit);
+    let len = (hi | lo) as usize;
+
+    let mut key = vec![0; len];
+    for i in 0..len {
+        key[i] = base36_diff(filter[0x510 + i], filter[0x110 + i]);
     }
 
-    let hi = base36_diff(filter[0x500], filter[0x100]) << 4;
-    let lo = base36_diff(filter[0x501], filter[0x101]);
-
-    (hi | lo) as usize
+    key
 }
 
 #[test]
-fn decode() {
-    let mut filter = filters::YUKI;
-    decode_filter(&mut filter);
+fn decode_isf() {
+    let mut filter = filters::TOSHIAKI;
+    decode(&mut filter);
+    let mut isf = std::fs::read("src/secretfilter/START.ISF").unwrap();
+    let mut key = get_key(&filter);
+    let key_len = key.len();
+
+    for (i, b) in &mut isf.iter_mut().enumerate() {
+        if i.is_multiple_of(key_len) {
+            update_key(&mut key, i, &filter);
+        }
+
+        *b ^= key[i % key_len];
+    }
+
+    std::fs::write("start.isf", isf).unwrap();
 }
