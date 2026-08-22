fba96dc551924ed98692aee255e886515c49bb44
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-08-22 18:18:05 +0700

    clippy

--- reikura_gdl/src/instruction/cns.rs
+++ reikura_gdl/src/instruction/cns.rs
@@ -7,7 +7,7 @@ pub fn cns(vm: &mut Vm, info: InstructionInfo) -> anyhow::Result<()> {
     let _: u8 = vm.parser.read_param()?;
     let index: u8 = vm.parser.read_param()?;
     let name_bytes = vm.parser.read_bytes(info.param_len - 2)?;
-    let name = sjis_to_utf8(&name_bytes)?;
+    let name = sjis_to_utf8(name_bytes)?;
 
     vm.ctx.char_names[index as usize] = Some(name);
 
