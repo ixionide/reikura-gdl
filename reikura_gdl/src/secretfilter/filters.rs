2471a01b5ff1ed7ae9b760f20279484087ae8315
Ixionide <200210767+ixionide@users.noreply.github.com>
2026-08-25 22:00:41 +0700

    add more decrypt keys

--- reikura_gdl/src/secretfilter/filters.rs
+++ reikura_gdl/src/secretfilter/filters.rs
@@ -1,13 +1,22 @@
-pub const YUKI: [u8; 2048] = *include_bytes!("yuki.filter");
-pub const UMINOEX: [u8; 2048] = *include_bytes!("uminoex.filter");
+pub const KANAOKA: [u8; 2048] = *include_bytes!("kanaoka.filter");
+pub const KZNR: [u8; 2048] = *include_bytes!("kznr.filter");
+pub const PURUC: [u8; 2048] = *include_bytes!("puruc.filter");
+pub const PURUCX: [u8; 2048] = *include_bytes!("purucx.filter");
 pub const TOSHIAKI: [u8; 2048] = *include_bytes!("toshiaki.filter");
+pub const UMINOEX: [u8; 2048] = *include_bytes!("uminoex.filter");
+pub const YUKI: [u8; 2048] = *include_bytes!("yuki.filter");
 
 pub fn get_known_filter(title_id: &str) -> Option<[u8; 2048]> {
-    match title_id {
-        "YUKI" => YUKI,
-        "UMINOEX" => UMINOEX,
+    let filter = match title_id {
+        "KANAOKA" => KANAOKA,
+        "KZNR" | "KZNRUS" => KZNR,
+        "PURUC" | "PURUCUS" => PURUC,
+        "PURUCX" | "PURUCXUS" => PURUC,
         "TOSHIAKI" => TOSHIAKI,
+        "UMINOEX" => UMINOEX,
+        "YUKI" | "YUKIUS" => YUKI,
         _ => return None,
-    }
-    .into()
+    };
+
+    Some(filter)
 }
